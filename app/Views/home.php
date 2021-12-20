c7c57eb66bea50b180b264ead510e402245c8179
Muhammad Taufik Prayitno <taufikprayitno16@gmail.com>
2021-12-20 23:20:36 +0700

    add base layout, nav & welcome page

---
+++ app/Views/home.php
@@ -0,0 +1,8 @@
+<?= $this->extend('layouts/template') ?>
+
+<?= $this->section('content') ?>
+<div class="container mt-4">
+    <h1>Welcome Kawan</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore vitae porro dolorum sunt dolores praesentium excepturi modi in cum doloribus! Ut aliquid culpa id fugit magnam deserunt repudiandae! Dolor ut doloribus quaerat maxime nisi, asperiores exercitationem dolorem illo tenetur, aliquid expedita ipsam ea impedit quo voluptates, pariatur ab veniam error sequi nobis architecto velit maiores iure. Molestias vel asperiores quam at iusto neque alias sunt dolore atque commodi dignissimos hic odit voluptates quis numquam accusamus cumque ea aut aperiam expedita, error magnam aliquam consequuntur nemo! Dolorum quidem repudiandae perferendis accusamus dicta fugit tempora, temporibus ratione. Soluta eius id rerum eligendi.</p>
+</div>
+<?= $this->endSection() ?>
